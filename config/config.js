7ee0f00b62d0c14e7116b76049a8862a9e6d7ad1
Chetan Shimpi <62544184+chetanshimpi141@users.noreply.github.com>
2025-03-04 01:35:17 +0530

    done

--- config/config.js
+++ config/config.js
@@ -1,26 +1,16 @@
 module.exports = {
   development: {
-    username: "postgres",
-    password: "uCUgvKLTGhVRAYYiowbpIgsAwKeDwkUX",
-    database: "railway",
-    host: "shortline.proxy.rlwy.net",
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "uCUgvKLTGhVRAYYiowbpIgsAwKeDwkUX",
+    database: process.env.DB_NAME || "railway",
+    host: process.env.DB_HOST || "shortline.proxy.rlwy.net",
     dialect: "postgres",
-    port: 39339,
-  },
-  test: {
-    username: "postgres",
-    password: "postgres",
-    database: "task_management_db_test",
-    host: "localhost",
-    dialect: "postgres",
-    port: 5432,
-  },
-  production: {
-    username: "postgres",
-    password: "postgres",
-    database: "task_management_db_prod",
-    host: "localhost",
-    dialect: "postgres",
-    port: 5432,
+    port: process.env.DB_PORT || 39339,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, 
+      }
+    }
   },
 };
